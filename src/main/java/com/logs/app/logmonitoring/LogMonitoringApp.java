f5ff1fdf335d90734670b24a76b5d88303fba333
Traistaru, Victor <vtraistaru@estee.com>
2025-04-10 01:43:09 +0300

    LOGS-09: Concurrent implementation of parsing logs and custom LogGenerator

--- src/main/java/com/logs/app/logmonitoring/LogMonitoringApp.java
+++ src/main/java/com/logs/app/logmonitoring/LogMonitoringApp.java
@@ -1,13 +1,17 @@
 package com.logs.app.logmonitoring;
 
+import static com.logs.app.logmonitoring.service.LogGenerator.generateLogs;
+
 import com.logs.app.logmonitoring.service.LogProcessor;
 
 public class LogMonitoringApp {
     public static void main(String[] args) {
         LogProcessor logProcessor = new LogProcessor();
 
-        // Specify the path to your logs.log file
-        String logFilePath = "src/main/resources/logs.log";
+        // Specify the path to desired logs.log file
+        //String logFilePath = "src/main/resources/logs.log";
+        generateLogs();
+        String logFilePath = "src/main/resources/massive_logs.logs";
 
         try {
             logProcessor.parseLogs(logFilePath);
--- src/main/java/com/logs/app/logmonitoring/service/LogProcessor.java
+++ src/main/java/com/logs/app/logmonitoring/service/LogProcessor.java
@@ -1,5 +1,7 @@
 package com.logs.app.logmonitoring.service;
 
+import static com.logs.app.logmonitoring.validation.LogFileValidation.validateEntry;
+
 import com.logs.app.logmonitoring.exception.InvalidLogEntryException;
 import com.logs.app.logmonitoring.model.ProcessJob;
 import lombok.Getter;
@@ -9,27 +11,46 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.logs.app.logmonitoring.validation.LogFileValidation.validateEntry;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Getter
 public class LogProcessor {
-    private final Map<Integer, List<ProcessJob>> processMap = new HashMap<>();
+    Map<Integer, List<ProcessJob>> processMap = Collections.synchronizedMap(new HashMap<>());
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
     public void parseLogs(String filePath) throws IOException, InvalidLogEntryException {
+        long startTime = System.currentTimeMillis();
         List<String> logEntries = Files.readAllLines(Paths.get(filePath));
+        List<Future<Void>> futures = new ArrayList<>();
 
         for (String logEntry : logEntries) {
-            validateEntry(logEntry);
-            ProcessJob process = createProcessJob(logEntry);
-            processMap.computeIfAbsent(process.getPid(), k -> new ArrayList<>()).add(process);
+            futures.add(executorService.submit(() -> {
+                validateEntry(logEntry);
+                ProcessJob process = createProcessJob(logEntry);
+                processMap.computeIfAbsent(process.getPid(), k -> new ArrayList<>()).add(process);
+                return null;
+            }));
+        }
+
+        try {
+            for (Future<Void> future : futures) {
+                future.get();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new InvalidLogEntryException("Log processing was interrupted.");
+        } catch (Exception e) {
+            throw new InvalidLogEntryException("Error processing log entry: " + e.getMessage());
         }
 
         computeDurationsAndStatuses();
+        long endTime = System.currentTimeMillis(); // End timer
+
+        System.out.println("Time taken for concurrent processing: " + (endTime - startTime) + " ms");
+        executorService.shutdown(); // Shutdown the executor service
     }
 
     private ProcessJob createProcessJob(String logEntry) {
